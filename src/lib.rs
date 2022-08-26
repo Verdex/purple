3165632067aea6a4e33553c9a2bb51bb06bd71d0
David L. Inman <davidlevere@gmail.com>
2022-08-25 22:08:27 -0400

    label map

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashMap;
+
 mod error;
 mod data;
 
@@ -9,6 +11,7 @@ struct Frame<'a, T : Clone + ToData<'a, T>> {
     instr_ptr : usize,
     locals : Locals<'a, T>,
     current_function : usize,
+    label_map : HashMap<Label, usize>,
 }
 
 
@@ -26,6 +29,7 @@ pub fn run<'a, T : Clone + ToData<'a, T>>( func_defs : &'a Vec<FuncDef<'a, T>>
     let mut instrs = &func_defs[current_function].body;
     let mut instr_ptr = 0;
     let mut locals : Locals<'a, T> = Locals::new();
+    let mut label_map : HashMap<Label, usize> = HashMap::new();
 
     loop {
 
@@ -34,14 +38,15 @@ pub fn run<'a, T : Clone + ToData<'a, T>>( func_defs : &'a Vec<FuncDef<'a, T>>
                 break;
             }
 
-            Frame { instr_ptr, locals, current_function } = stack.pop().unwrap();
+            Frame { instr_ptr, locals, current_function, label_map } = stack.pop().unwrap();
             // NOTE:  We don't have to check if current_function exists because if we're poping
             // then we must have called it previously.
             instrs = &func_defs[current_function].body;
         }
 
         /*match instrs[instr_ptr] {
-            Instr::Label(Label),
+            Instr::Label(Label) =>
+
             Instr::Jump(Label),
             Instr::BranchOnTrue(Label, Box<dyn FnMut(&Locals<'a, T>, &'a Vec<Data<'a, T>>) -> Result<bool, Box<dyn std::error::Error>>>),
             Instr::Return(Symbol),
